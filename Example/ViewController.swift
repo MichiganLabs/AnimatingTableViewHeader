ed8a260906f125de8c7efa5515fc0383a79e3cc9
John DeLong <jdelong@michiganlabs.com>
2016-05-28 10:23:45 -0400

    Remove stub methods

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -28,11 +28,6 @@ class ViewController: UIViewController {
         self.tableView.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         self.headerHeightConstraint.constant = self.maxHeaderHeight
@@ -136,4 +131,3 @@ extension ViewController: UITableViewDelegate {
         self.logoImageView.alpha = percentage
     }
 }
-
